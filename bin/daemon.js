8fe1997173f9470b93de886b832d8b282b526931
Ilia Potuzhnov <ipotuzhnov@gmail.com>
2017-11-10 23:37:41 -0500

    deamon: send commits

--- bin/daemon.js
+++ bin/daemon.js
@@ -5,6 +5,6 @@ const socket = require('socket.io-client')(url)
 
 socket.on('connect', () => {
     console.log("i received a connection")
-    socket.emit('changes', "somedata")
+    socket.emit('commit', "somedata")
 });
 
