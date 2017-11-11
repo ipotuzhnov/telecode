e436d2c6ae721fb0c3dd278278529ef17960bd92
Ilia Potuzhnov <ipotuzhnov@gmail.com>
2017-11-11 14:40:09 -0500

    web editor: add function to remove/replace/insert a line

--- public/js/editor.js
+++ public/js/editor.js
@@ -2,6 +2,51 @@
 Editor = (() => {
     const Editor = { editor: null }
 
+    require.config({ paths: { 'vs': '/node_modules/monaco-editor/min/vs' }})
+
+    require(['vs/editor/editor.main'], async () => {
+        const file = await fetch('/index.html')
+        const content = await file.text()
+
+        const model = monaco.editor.createModel(content, 'html')
+        Editor.model = model
+
+        const editor = monaco.editor.create(
+            document.getElementById('container'),
+            { model: Editor.model }
+        )
+        Editor.editor = editor
+
+
+        // insert line
+        function insertLine (number, text) {
+            model.applyEdits([{ range: new monaco.Range(
+                number, 1,
+                number, 1
+            ), text: text + '\n' }])
+        }
+        Editor.insertLine = insertLine
+
+        // replace line
+        function replaceLine (number, text) {
+            model.applyEdits([{ range: new monaco.Range(
+                number, 1,
+                number + 1, 1
+            ), text: text + '\n' }])
+        }
+        Editor.replaceLine = replaceLine
+
+        // remove line
+        function removeLine (number) {
+            model.applyEdits([{ range: new monaco.Range(
+                number, 1,
+                number + 1, 1
+            ), text: '' }])
+        }
+        Editor.removeLine = removeLine
+    })
+
+    // highlight line
     // setTimeout(() => {
     //     if (!Editor.editor) return
 
@@ -19,20 +64,6 @@ Editor = (() => {
     //         }])
     //     })
     // }, 2000)
-
-    require.config({ paths: { 'vs': '/node_modules/monaco-editor/min/vs' }})
-
-    require(['vs/editor/editor.main'], async () => {
-        const file = await fetch("/index.html")
-        const content = await file.text()
-
-        Editor.model = monaco.editor.createModel(content, 'html')
-
-        Editor.editor = monaco.editor.create(
-            document.getElementById('container'),
-            { model: Editor.model }
-        )
-    })
 
     return Editor
 })()
