f8dbc7229998019050c625b3664e3901897d1b6a
edubrovs <ethel.dubrovsky@livestream.com>
2017-11-10 23:55:13 -0500

    sockets

--- bin/daemon.js
+++ bin/daemon.js
@@ -7,15 +7,15 @@ sockets.forEach(i => {
     const socket = require('socket.io-client')(url)
 
     socket.on('connect', () => {
-        console.log("i received a connection")
+        console.log(`${socket.id} received a connection`)
         socket.emit('commit', {"somevalue": "somekey"})
     })
 
     socket.on('update', data => {
         if (data.authorId === socket.id) {
-            console.log(socket.id + " Ignorning own updates for " + JSON.stringify(data))
+            console.log(`${socket.id} Ignorning own updates of ${JSON.stringify(data)}`)
         } else {
-            console.log(socket.id + " Received updates " + JSON.stringify(data))
+            console.log(`${socket.id} Received updates of ${JSON.stringify(data)}`)
         }
     })
 })
