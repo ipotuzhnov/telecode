ad06c2b1629246be2e03df6e1100be73415b1074
Richard Marmorstein <richard.marmorstein@livestream.com>
2017-11-11 01:24:17 -0500

    Factor out db.js

---
+++ src/db.js
@@ -0,0 +1,27 @@
+'use strict'
+const util      = require('util')
+const exec      = util.promisify(require('child_process').exec)
+const writeFile = util.promisify(require('fs').writeFile)
+
+module.exports = function DB ({dir}) {
+    async function dbExec (cmd) {
+        return await exec(cmd, { cwd: dir})
+    }
+    return {
+        version: null,
+        incrementVersion () {
+            this.version++
+        },
+        async init () {
+            this.version = 0
+            return await exec(`mkdir -p ${dir}`)
+        },
+        async update (patch) {
+            const tmpFileName = `tmp.${Date.now()}`
+            await writeFile(`${dir}/${tmpFileName}`, patch)
+            const ret = await dbExec(`cat ${tmpFileName} | patch`)
+            await dbExec(`rm ${tmpFileName}`)
+            return ret
+        }
+    }
+}
