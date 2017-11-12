bb714bd595a2f829a5fba0d6012090a387b83857
Richard Marmorstein <richard.marmorstein@livestream.com>
2017-11-12 17:05:30 -0500

    Awesome coloring

--- public/js/sio.js
+++ public/js/sio.js
@@ -2,12 +2,12 @@
 SIO = (() => {
     const url = location.origin
     const SIO = { socket: null }
-    SIO.requestId = Date.now();
+    SIO.requestId = Date.now()
     
-    const socket = io(url);
-    SIO.socket = socket;
+    const socket = io(url)
+    SIO.socket = socket
     $(function () {
-        SIO.socket.on("connect", function () {
+        SIO.socket.on('connect', function () {
             console.log('connected')
         })
         // SIO.socket.on('change', function(msg){
@@ -16,34 +16,36 @@ SIO = (() => {
     })
 
     SIO.joinRoom = function () {
-        const room = document.getElementById("room").value;
+        document.getElementById('intro').style.display='none'
+        document.getElementById('content').style.display='block'
+        const room = document.getElementById('room').value
         socket.emit('room', { room })
         socket.on('joined', async () => {
-            document.getElementById("room-chooser").style.display="none";
-            document.getElementById("room-message").innerHTML = `Congratulations, you've joined room ${room}.`;
-            document.getElementById("file-chooser").style.display="block";
-            console.log("Joined");
+            document.getElementById('room-chooser').style.display='none'
+            document.getElementById('room-message').innerHTML = `Congratulations, you've joined room ${room}.`
+            document.getElementById('file-chooser').style.display='block'
+            console.log('Joined')
         })
     }
 
     SIO.retrieveFile = function () {
-        SIO.requestId = Date.now(); 
-        var fileName = document.getElementById("file").value;
-        console.log(`Retrieving ${fileName}`);
-        Editor.resetFile(fileName);
+        SIO.requestId = Date.now()
+        const fileName = document.getElementById('file').value
+        console.log(`Retrieving ${fileName}`)
+        Editor.resetFile(fileName)
         socket.emit('retrieve_file', {
             name: fileName, requestId: SIO.requestId
         })
     }
 
     SIO.sendFile = function () {
-        const file = Editor.getFile();
-        console.log(`Sending file ${JSON.stringify(file)}`);
+        const file = Editor.getFile()
+        console.log(`Sending file ${JSON.stringify(file)}`)
         if (!file.name || !file.content) {
-            console.log("error on file send");
-            return;
+            console.log('error on file send')
+            return
         }
-        socket.emit("file_changed", file);
+        socket.emit('file_changed', file)
     }
 
     return SIO
