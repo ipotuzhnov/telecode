9e0372adcb028b0c700b05f0f74176cfd19de1f5
Richard Marmorstein <richard.marmorstein@livestream.com>
2017-11-12 12:01:00 -0500

    Rooms

--- bin/daemon.js
+++ bin/daemon.js
@@ -8,59 +8,64 @@ const url    = process.env['URL'] || 'https://nodeist-colony.herokuapp.com/'
 const gitUrl = process.env['GIT_URL'] || 'localhost'
 const REPO_DIR = `/tmp/nodeist-${Date.now()}`
 
+const ROOM = process.env['ROOM'] || `room-${Date.now()}`
+
 const socket = require('socket.io-client')(url)
 
 let startWatcher, stopWatcher
 // added a comment
 console.log(`attempting to connect to ${url}`)
 socket.on('connect', () => {
-    socket.emit('replay')
-    let isMaster = false
-    socket.on('replay', async commits => {
-
-        function sendCommit (diff) {
-            const data = {author: socket.id, diff}
-            console.log('emitting', data)
-            socket.emit('commit', data)
-        }
-        if (commits.length === 0) {
-            console.log('I am the master')
-            isMaster = true
-        } else {
-            console.log('I am not the master')
-        }
-
-        await sync.initRepo(REPO_DIR)
-
-        if (isMaster) {
-            const diff = await sync.syncDirToRepo(process.cwd(), REPO_DIR)
-            sendCommit(diff)
-        }
-
-        for (const commit of commits) {
-            console.log('applying change: ', commit)
-            await applyChange(commit)
-            console.log('done applying change')
-        }
-
-        {
-            let watcherId
-            startWatcher = async () => {
-                watcherId = Date.now()
-                console.log('starting watcher', watcherId)
-                await sync.watch(process.cwd(), REPO_DIR, watcherId, diff => { sendCommit(diff) })
+    console.log(`Joining room ${ROOM}...`)
+    socket.emit('room', ROOM)
+    socket.on('joined', () => {
+        socket.emit('replay')
+        let isMaster = false
+        socket.on('replay', async commits => {
+            function sendCommit (diff) {
+                const data = {author: socket.id, diff}
+                console.log('emitting', data)
+                socket.emit('commit', data)
             }
-            stopWatcher = () => {
-                console.log('stopping watcher', watcherId)
-                sync.unwatch(process.cwd())
+            if (commits.length === 0) {
+                console.log('I am the master')
+                isMaster = true
+            } else {
+                console.log('I am not the master')
             }
-        }
-        startWatcher()
-    })
 
-    socket.on('retrieve_file', async ({ requestId, name }) => {
-        const content = await readFile(`./${name}`, 'utf8')
-        socket.emit('file_retrieved', { requestId, name, content })
+            await sync.initRepo(REPO_DIR)
+
+            if (isMaster) {
+                const diff = await sync.syncDirToRepo(process.cwd(), REPO_DIR)
+                sendCommit(diff)
+            }
+
+            for (const commit of commits) {
+                console.log('applying change: ', commit)
+                await applyChange(commit)
+                console.log('done applying change')
+            }
+
+            {
+                let watcherId
+                startWatcher = async () => {
+                    watcherId = Date.now()
+                    console.log('starting watcher', watcherId)
+                    await sync.watch(process.cwd(), REPO_DIR, watcherId, diff => { sendCommit(diff) })
+                }
+                stopWatcher = () => {
+                    console.log('stopping watcher', watcherId)
+                    sync.unwatch(process.cwd())
+                }
+            }
+            startWatcher()
+        })
+
+        socket.on('retrieve_file', async ({ requestId, name }) => {
+            const content = await readFile(`./${name}`, 'utf8')
+            socket.emit('file_retrieved', { requestId, name, content })
+        })
     })
 })
 
