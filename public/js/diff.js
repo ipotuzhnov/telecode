cde9497c141443c61314851c28c77399848278a2
Richard Marmorstein <richard.marmorstein@livestream.com>
2017-11-12 18:01:33 -0500

    Prettier diff

--- public/js/diff.js
+++ public/js/diff.js
@@ -3,13 +3,13 @@
 GitDiff = (() => {
 
     function date_diff_indays (dt1, dt2) {
-        return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+        return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24))
     }
 
     // const Notifications = initNotifcations();
-    const DiffTitleDiv = document.getElementById('diff-title');
-    const DiffDiv = document.getElementById('diff-content');
-    const now = new Date();
+    const DiffTitleDiv = document.getElementById('diff-title')
+    const DiffDiv = document.getElementById('diff-content')
+    const now = new Date()
 
     class GitDiff {
         static getJSONFromDiff (gitDiff) {
@@ -17,16 +17,16 @@ GitDiff = (() => {
             return Diff2Html.getJsonFromDiff(gitDiff)
         }
         static getPrettyHtmlFromDiff (gitDiff) {
-            const output =  Diff2Html.getPrettyHtmlFromDiff(gitDiff);
-            const date = new Date();
-            const daysDiff = date_diff_indays(now, date) 
+            const output =  Diff2Html.getPrettyHtmlFromDiff(gitDiff)
+            const date = new Date()
+            const daysDiff = date_diff_indays(now, date)
             const when = daysDiff === 0 ? `today at ${
-                [date.getHours(), date.getMinutes(), date.getSeconds()].join(":")}` : `${daysDiff} days ago`;
-            DiffTitleDiv.innerHTML = `Your fellow developer pushed a change ${when}`;
-            DiffDiv.innerHTML = output;                
-            document.getElementById("diff").style.display="block";
+                [date.getHours(), date.getMinutes(), date.getSeconds()].join(':')}` : `${daysDiff} days ago`
+            DiffTitleDiv.innerHTML = `Your fellow developer pushed a change ${when}`
+            DiffDiv.innerHTML = output
+            document.getElementById('diff').style.display='block'
             Notifications.pushTestNotification('Your fellow developer has pushed a change. Applying the change!')
-            return output;
+            return output
         }
         static async loadDiffFromURL (url) {
             const body = await fetch(url)
